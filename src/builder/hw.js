f9a672e6937bfd9c844be64326d44b796b6c4101
Sergei Komarov <komarovsmail33@gmail.com>
2019-12-22 17:10:56 +0300

    add good solution

--- src/builder/hw.js
+++ src/builder/hw.js
@@ -1,26 +1,73 @@
-class CodeBuilder {
-  constructor(className) {
-    this.className = className
-    this.fields = []
-  }
+//bad variant
+// class CodeBuilder {
+//   constructor(className) {
+//     this.className = className;
+//     this.fields = [];
+//   }
 
-  addField(name) {
-    this.fields.push(name)
-    return this
-  }
+//   addField(name) {
+//     this.fields.push(name);
+//     return this;
+//   }
 
-  toString() {
-    return (this.fields.length > 0) 
-            ? `class ${this.className}\n`
-            + `  constructor(${this.fields[0]}, ${this.fields[1]}) {\n`
-            + `    this.${this.fields[0]} = ${this.fields[0]};\n`
-            + `    this.${this.fields[1]} = ${this.fields[1]};\n`
-            + `  }\n`
-            + `}`
-            : `class ${this.className}\n{\n}`
+//   toString() {
+//     return this.fields.length > 0
+//       ? `class ${this.className} {\n` +
+//           `  constructor(${this.fields[0]}, ${this.fields[1]}) {\n` +
+//           `    this.${this.fields[0]} = ${this.fields[0]};\n` +
+//           `    this.${this.fields[1]} = ${this.fields[1]};\n` +
+//           `  }\n` +
+//           `}`
+//       : `class ${this.className} {\n}`;
+//   }
+// }
+
+class Field {
+  constructor(name) {
+    this.name = name;
   }
 }
 
-let cb = new CodeBuilder('Person')
-// cb.addField('name').addField('age')
-console.log(cb.toString())
+class Class {
+  constructor(name) {
+    this.name = name;
+    this.fields = [];
+  }
+
+  toString() {
+    let buffer = [];
+    buffer.push(`class ${this.name} {\n`);
+
+    if (this.fields.length > 0) {
+      buffer.push(`  constructor(`);
+      this.fields.forEach((elem, indx) => {
+        buffer.push(elem.name);
+        if (indx + 1 !== this.fields.length) buffer.push(", ");
+      });
+      buffer.push(`) {\n`);
+      this.fields.forEach(elem => buffer.push(`    this.${elem.name} = ${elem.name};\n`));
+      buffer.push("  }\n");
+      buffer.push("}");
+    }
+    return buffer.join("");
+  }
+}
+
+class CodeBuilder {
+  constructor(className) {
+    this._class = new Class(className);
+  }
+
+  addField(name) {
+    this._class.fields.push(new Field(name));
+    return this;
+  }
+
+  toString() {
+    return this._class.toString();
+  }
+}
+
+let cb = new CodeBuilder("Person");
+cb.addField("name").addField("age");
+console.log(cb.toString());
--- src/solid/2-open-closed-principle.js
+++ src/solid/2-open-closed-principle.js
@@ -1,38 +1,37 @@
-const {log} = require('utils/index')
+const { log } = require("..");
 
 let Color = Object.freeze({
-  red: 'red',
-  green: 'green',
-  blue: 'blue',
-})
+  red: "red",
+  green: "green",
+  blue: "blue"
+});
 
 let Size = Object.freeze({
-  small: 'small',
-  medium: 'medium',
-  large: 'large',
-})
+  small: "small",
+  medium: "medium",
+  large: "large"
+});
 
 class Product {
   constructor(name, color, size) {
-    this.name = name
-    this.color = color
-    this.size = size
+    this.name = name;
+    this.color = color;
+    this.size = size;
   }
 }
 
 // open for extension, closed for modification
 class ProductFilter {
   filterByColor(products, color) {
-    return products.filter(p => p.color === color)
+    return products.filter(p => p.color === color);
   }
 
   filterBySize(products, size) {
-    return products.filter(p => p.size === size)
+    return products.filter(p => p.size === size);
   }
 
   filterBySizeAndColor(products, size, color) {
-    return products.filter(p => 
-      p.size === size && p.color === color)
+    return products.filter(p => p.size === size && p.color === color);
   }
 
   // state space explosion
@@ -41,8 +40,7 @@ class ProductFilter {
 // specifications
 class Specification {
   constructor() {
-    if (this.constructor.name === 'Specification')
-      throw new Error('Specification is abstract!')
+    if (this.constructor.name === "Specification") throw new Error("Specification is abstract!");
   }
 
   isSatisfied(item) {}
@@ -50,65 +48,63 @@ class Specification {
 
 class ColorSpecification extends Specification {
   constructor(color) {
-    super()
-    this.color = color
+    super();
+    this.color = color;
   }
 
   isSatisfied(item) {
-    return item.color === this.color
+    return item.color === this.color;
   }
 }
 
-class SizeSpecification  extends Specification {
+class SizeSpecification extends Specification {
   constructor(size) {
-    super()
-    this.size = size
+    super();
+    this.size = size;
   }
 
   isSatisfied(item) {
-    return item.size === this.size
+    return item.size === this.size;
   }
 }
 
-class AndSpecification  extends Specification {
+class AndSpecification extends Specification {
   constructor(...specs) {
-    super()
-    this.specs = specs
+    super();
+    this.specs = specs;
   }
 
   isSatisfied(item) {
-    return this.specs.every(x => x.isSatisfied(item))
+    return this.specs.every(x => x.isSatisfied(item));
   }
 }
 
 class BetterFilter {
   filter(items, spec) {
-    return items.filter(x => spec.isSatisfied(x))
+    return items.filter(x => spec.isSatisfied(x));
   }
 }
 
-let apple = new Product('Apple', Color.green, Size.small)
-let tree = new Product('Tree', Color.green, Size.large)
-let house = new Product('House', Color.blue, Size.large)
+let apple = new Product("Apple", Color.green, Size.small);
+let tree = new Product("Tree", Color.green, Size.large);
+let house = new Product("House", Color.blue, Size.large);
 
-let products = [apple, tree, house]
-let pf = new ProductFilter()
-log(`Green products (old):`)
-for(let p of pf.filterByColor(products, Color.green))
-  log(` * ${p.name} is green`)
+let products = [apple, tree, house];
+let pf = new ProductFilter();
+log(`Green products (old):`);
+for (let p of pf.filterByColor(products, Color.green)) log(` * ${p.name} is green`);
 
-let bf = new BetterFilter()
-log('Green products (new):')
-for(let p of bf.filter(products,
-  new ColorSpecification(Color.green))) {
-    log(` * ${p.name} is green`)
+let bf = new BetterFilter();
+log("Green products (new):");
+for (let p of bf.filter(products, new ColorSpecification(Color.green))) {
+  log(` * ${p.name} is green`);
 }
 
-log('Large and green products:')
+log("Large and green products:");
 let spec = new AndSpecification(
   new ColorSpecification(Color.green),
   new SizeSpecification(Size.large)
-)
+);
 for (let p of bf.filter(products, spec)) {
-  log(` * ${p.name} is large and green`)
+  log(` * ${p.name} is large and green`);
 }
